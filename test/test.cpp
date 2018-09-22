3d20c8f38bb9c4ae18391fa38bb163ed3015b88e
Adarsh Jagan S <adarshjagan3895@gmail.com>
2018-09-22 09:19:59 -0400

    test/test: Added test cases to check error and velocity calculation.

--- test/test.cpp
+++ test/test.cpp
@@ -1,5 +1,14 @@
 #include <gtest/gtest.h>
+#include "PIDcontroller.hpp"
 
-TEST(dummy, should_pass) {
-  EXPECT_EQ(1, 1);
+TEST(PIDtest1, checkErrorCalculation) {
+  PIDcontroller pid1(0.1, 0.1, 0.1, 2.0, 1.0);
+  PIDcontroller pid2(0.1, 0.1, 0.1, 1.0, 1.5);
+  ASSERT_DOUBLE_EQ(pid1.computeError(), -1);  //Error = (desiredVel - currentVel)
+  ASSERT_DOUBLE_EQ(pid2.computeError(), 0.5);
+}
+
+TEST(PIDtest2, checkVelocityCalculation) {
+  PIDcontroller pid(0.1, 0.1, 0.1, 1.0, 2.0);
+  EXPECT_NEAR(pid.computeVelocity(), 2.0, 0.001);
 }
